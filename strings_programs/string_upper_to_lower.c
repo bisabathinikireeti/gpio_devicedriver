b210dff7bf2c35fcb3b2fd26dbe7e97c49ebd1f1
Bisabathini Kireeti <bisabathinikireeti@gmail.com>
2026-03-05 11:43:14 +0530

    Create string_upper_to_lower.c

---
+++ strings_programs/string_upper_to_lower.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    char str[50];
+    fgets(str,sizeof(str),stdin);
+    printf("%s\n",str);
+    int len=0;
+    while(str[len] != '\0')
+    len++;
+    
+    for(int i=0;i<len;i++)
+    {
+        if(str[i]>=65 && str[i]<=97)
+            str[i]=str[i]+32;
+    }
+    
+    printf("%s is lower alphabets\n",str);
+    return 0;
+}
