b4f43a4d1468c64bd345cf4b3d8f5b71cc88d227
Bisabathini Kireeti <bisabathinikireeti@gmail.com>
2026-04-20 15:13:37 +0530

    Update Find position of the only set bit.c

--- bit_manipulation _codes/Find position of the only set bit.c
+++ bit_manipulation _codes/Find position of the only set bit.c
@@ -1,3 +1,4 @@
+//normal way
 #include<stdio.h>
 void main()
 {
@@ -20,4 +21,25 @@ void main()
     else
     printf("%d ",-1);
    
+}
+
+//bit manipultion way
+#include<stdio.h>
+int  main()
+{
+    printf("enter the number\n");
+    int n,index;
+    scanf("%d",&n);
+    if(n>0 && (n&(n-1))!=0){
+    printf("%d",-1);
+    return 0;
+    }
+    int pos=1;
+    while(n>1)
+    {
+        n=n>>1;
+        pos++;
+    }
+   printf("%d",pos);
+   return 0;
 }
